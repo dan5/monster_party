0441b59b8f48afdbffac101f76992a1ae0c456b8
dan5 <dan5.ya@gmail.com>
2012-04-23 18:40:47 +0900

    rails new monster_party -m http://railswizard.org/de61594e586c57135f8d.rb -J -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonsterParty::Application.config.secret_token = '5e398ea38bf5fdc15a584891bc9176ecef5677f4a060bd43ca3ba75ea6f969a60a716187410b5b6d24b4b71fc9636a2262fc0cd7cc1ee8d9dc394c33313a32a9'
